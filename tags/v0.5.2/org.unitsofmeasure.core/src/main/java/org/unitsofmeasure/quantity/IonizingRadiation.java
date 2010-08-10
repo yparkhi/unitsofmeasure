b4b2ce2fc64d4b874c027d893d9a8b6101b0f2b1
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2010-08-10 04:27:58 +0000

    NEW - task 1: Check artifactId http://code.google.com/p/unitsofmeasure/issues/detail?id=1

---
+++ tags/v0.5.2/org.unitsofmeasure.core/src/main/java/org/unitsofmeasure/quantity/IonizingRadiation.java
@@ -0,0 +1,25 @@
+/**
+ *  The Units of Measure API, UCUM codes, UCUM table (regardless of format),
+ *  and UCUM Specification are copyright © 1999-2010, Regenstrief Institute, Inc.
+ *  and the Unified Codes for Units of Measures (UCUM) Organization.
+ *  All rights reserved.
+ *
+ * See TermsOfUse for details.
+ */
+package org.unitsofmeasure.quantity;
+
+import org.unitsofmeasure.Quantity;
+
+/**
+ * This interface represents the quantity of subatomic particles or
+ * electromagnetic waves that are energetic enough to detach electrons from
+ * atoms or molecules, ionizing them. The system unit for this quantity is
+ * "C/kg ("coulomb per kilogram).
+ * 
+ * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
+ * @version 1.2, $Date$
+ * @see <a href="http://en.wikipedia.org/wiki/Ionizing_radiation">Wikipedia's
+ *      Ionizing Radiation</a>
+ */
+public interface IonizingRadiation extends Quantity<IonizingRadiation> {
+}
---
+++ tags/v0.5.2/org.unitsofmeasure.core/src/main/java/org/unitsofmeasure/util/MetricPrefix.java
@@ -0,0 +1,326 @@
+/**
+ * The Units of Measure API, UCUM codes, UCUM table (regardless of format),
+ * and UCUM Specification are copyright © 1999-2010,
+ * Regenstrief Institute, Inc. and the Unified Codes for Units of Measures
+ * (UCUM) Organization. All rights reserved.
+ *
+ * See TermsOfUse for details.
+ */
+package org.unitsofmeasure.util;
+
+import org.unitsofmeasure.Quantity;
+import org.unitsofmeasure.Unit;
+
+/**
+ * <p>
+ * This class provides support for the 20 SI prefixes used in the metric system
+ * (decimal multiples and submultiples of SI units). For example:
+ * 
+ * <pre>
+ * <code>
+ *     import static my.unitsofmeasure.impl.SI.*;       // Static import.
+ *     import static org.unitsofmeasure.MetricPrefix.*; // Static import.
+ *     import org.unitsofmeasure.Unit;   
+ *     import org.unitsofmeasure.quantity.*;
+ *     ...
+ *     Unit<Pressure> HECTOPASCAL = HECTO(PASCAL);
+ *     Unit<Length> KILOMETRE = KILO(METRE);
+ * </pre>
+ * 
+ * </code>
+ * </p>
+ * 
+ * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
+ * @author <a href="mailto:jcp@catmedia.us">Werner Keil</a>
+ * @see <a
+ *      href="http://en.wikipedia.org/wiki/International_System_of_Units">Wikipedia:
+ *      International System of Units</a>
+ * @version 1.0.1 ($Revision$), $Date$
+ */
+public final class MetricPrefix {
+
+    private MetricPrefix() {
+	// Utility class no visible constructor.
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>24</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e24)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> YOTTA(Unit<Q> unit) {
+	return unit.multiply(1E24);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>21</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e21)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> ZETTA(Unit<Q> unit) {
+	return unit.multiply(1E21);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>18</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e18)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> EXA(Unit<Q> unit) {
+	return unit.multiply(1E18);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>15</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e15)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> PETA(Unit<Q> unit) {
+	return unit.multiply(1E15);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>12</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e12)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> TERA(Unit<Q> unit) {
+	return unit.multiply(1E12);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>9</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e9)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> GIGA(Unit<Q> unit) {
+	return unit.multiply(1E9);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>6</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e6)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> MEGA(Unit<Q> unit) {
+	return unit.multiply(1E6);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>3</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e3)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> KILO(Unit<Q> unit) {
+	return unit.multiply(1E3);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>2</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e2)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> HECTO(Unit<Q> unit) {
+	return unit.multiply(1E2);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>1</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.multiply(1e1)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> DEKA(Unit<Q> unit) {
+	return unit.multiply(1E1);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-1</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e1)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> DECI(Unit<Q> unit) {
+	return unit.divide(1E1);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-2</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e2)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> CENTI(Unit<Q> unit) {
+	return unit.divide(1E2);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-3</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e3)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> MILLI(Unit<Q> unit) {
+	return unit.divide(1E3);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-6</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e6)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> MICRO(Unit<Q> unit) {
+	return unit.divide(1E6);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-9</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e9)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> NANO(Unit<Q> unit) {
+	return unit.divide(1E9);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-12</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e12)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> PICO(Unit<Q> unit) {
+	return unit.divide(1E12);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-15</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e15)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> FEMTO(Unit<Q> unit) {
+	return unit.divide(1E15);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-18</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e18)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> ATTO(Unit<Q> unit) {
+	return unit.divide(1E18);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-21</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e21)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> ZEPTO(Unit<Q> unit) {
+	return unit.divide(1E21);
+    }
+
+    /**
+     * Returns the specified unit multiplied by the factor
+     * <code>10<sup>-24</sup></code>
+     * 
+     * @param <Q>
+     *            The type of the quantity measured by the unit.
+     * @param unit
+     *            any unit.
+     * @return <code>unit.divide(1e24)</code>.
+     */
+    public static <Q extends Quantity<Q>> Unit<Q> YOCTO(Unit<Q> unit) {
+	return unit.divide(1E24);
+    }
+
+}
