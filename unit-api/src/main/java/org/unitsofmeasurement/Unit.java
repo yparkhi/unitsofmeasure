4a5d6a5844662466505fd54fbe08418f8e6e4f64
dautelle <dautelle@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2010-09-10 16:43:06 +0000

    Fixed Test and Javadoc errors.

--- unit-api/src/main/java/org/unitsofmeasurement/Unit.java
+++ unit-api/src/main/java/org/unitsofmeasurement/Unit.java
@@ -19,7 +19,7 @@ import org.unitsofmeasurement.quantity.Quantity;
  * which they are created. Thus, for example, the string "g/kg" (which is a
  * dimensionless unit) would result from invoking the method
  * toString() on a unit that was created by dividing a gram unit by a kilogram
- * unit. Yet, "kg" divided by "kg" returns {@link #ONE} and not "kg/kg" due to
+ * unit. Yet, "kg" divided by "kg" returns <code>ONE</code> and not "kg/kg" due to
  * automatic unit factorization.</p>
  * 
  * <p> This interface supports the multiplication of offsets units. The result is
