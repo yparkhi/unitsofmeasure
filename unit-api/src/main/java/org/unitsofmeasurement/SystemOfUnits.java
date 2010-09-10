64c20adf793e79abbb0a8186911851375e8204a0
dautelle <dautelle@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2010-09-10 16:02:34 +0000

    OSGi Bundlelization.

--- unit-api/src/main/java/org/unitsofmeasurement/SystemOfUnits.java
+++ unit-api/src/main/java/org/unitsofmeasurement/SystemOfUnits.java
@@ -7,53 +7,42 @@
  */
 package org.unitsofmeasurement;
 
-import java.util.HashSet;
 import java.util.Set;
 
 /**
- * <p>
- * This class represents a system of units, it groups units together for
+ * <p> This class represents a system of units, it groups units together for
  * historical or cultural reasons. Nothing prevents a unit from belonging to
  * several systems of units at the same time (for example an imperial system
- * would have many of the units held by {@link USCustomary}).
- * </p>
+ * would have many of the units held by <code>USCustomary</code>).</p>
  * 
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:jcp@catmedia.us">Werner Keil</a>
  * @version 1.2 ($Revision: 169 $), $Date: 2010-02-21 18:48:40 +0100 (So, 21
  *          Feb 2010) $
  */
-public abstract class SystemOfUnits {
+public interface SystemOfUnits {
 
-	/**
-	 * Default constructor.
-	 */
-	protected SystemOfUnits() {
-	}
+    /**
+     * Returns the name of the system of unit.
+     *
+     * @return the system of unit name.
+     */
+    String getName();
 
-	/**
-	 * Returns a read only view over the units defined in this system.
-	 * 
-	 * @return the collection of units.
-	 */
-	public abstract Set<Unit<?>> getUnits();
+    /**
+     * Returns a read only view over the units defined in this system.
+     *
+     * @return the collection of units.
+     */
+    Set<Unit<?>> getUnits();
 
-	/**
-	 * Returns the units defined in this system having the specified dimension
-	 * (convenience method). This method returns all the units in this system of
-	 * units having the specified dimension.
-	 * 
-	 * @param dimension
-	 *            the dimension of the units to be returned.
-	 * @return the collection of units of specified dimension.
-	 */
-	public Set<Unit<?>> getUnits(Dimension dimension) {
-		final Set<Unit<?>> units = new HashSet<Unit<?>>();
-		for (Unit<?> unit : getUnits()) {
-			if (unit.getDimension().equals(dimension)) {
-				units.add(unit);
-			}
-		}
-		return units;
-	}
+    /**
+     * Returns the units defined in this system having the specified dimension
+     * (convenience method).
+     *
+     * @param dimension the dimension of the units to be returned.
+     * @return the collection of units of specified dimension.
+     */
+    Set<Unit<?>> getUnits(Dimension dimension);
+
 }
