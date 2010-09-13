ace1603f8cbdcc8f52ce71a8feb74e0e3952efec
dautelle <dautelle@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2010-09-13 16:42:37 +0000

    Changes as per discussion.

---
+++ unit-api/src/main/java/org/unitsofmeasurement/quantity/Information.java
@@ -0,0 +1,21 @@
+/**
+ * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
+ * Copyright (c) 2005-2010, Unit-API contributors, JScience and others
+ * All rights reserved.
+ *
+ * See LICENSE.txt for details.
+ */
+package org.unitsofmeasurement.quantity;
+
+
+/**
+ * Represents a measure of information. The metric system unit for this quantity is
+ * "bit".
+ * 
+ * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
+ * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
+ * @version 1.1 ($Revision$), $Date$
+ */
+public interface Information extends Quantity<Information> {
+
+}
