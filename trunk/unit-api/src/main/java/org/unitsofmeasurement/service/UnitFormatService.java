b2e0e11e835faf247277020a2287c39fb9c3f31c
dautelle <dautelle@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2010-09-17 16:15:29 +0000

    Changes as per "[unitsofmeasure] Service Factories" discussion.

---
+++ trunk/unit-api/src/main/java/org/unitsofmeasurement/service/UnitFormatService.java
@@ -0,0 +1,49 @@
+/**
+ * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
+ * Copyright (c) 2005-2010, Unit-API contributors, JScience and others
+ * All rights reserved.
+ *
+ * See LICENSE.txt for details.
+ */
+package org.unitsofmeasurement.service;
+
+import java.util.Locale;
+import org.unitsofmeasurement.unit.UnitFormat;
+
+/**
+ * <p> This interface represent the service to obtain {@link UnitFormat}
+ *     instances.</p>
+ *
+ * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
+ * @see <a href="http://aurora.regenstrief.org/~ucum/ucum.html">UCUM Full Specification</a>
+ * @version 1.0
+ */
+public interface UnitFormatService extends UnitFormat {
+
+    /**
+     * Returns the default <a href="http://www.unitsofmeasure.org/">UCUM</a>
+     * unit format.
+     *
+     * @return <code>getUnitFormat("UCUM")</code>
+     */
+    UnitFormat getUnitFormat();
+
+    /**
+     * Returns the unit format having the specified name or
+     * <code>null</code> if none.
+     *
+     * @param name the name of the format.
+     * @return the corresponding unit format.
+     */
+    UnitFormat getUnitFormat(String name);
+
+    /**
+     * Returns the unit format for the specified locale or
+     * <code>null</code> if none.
+     *
+     * @param locale the locale for the format.
+     * @return the corresponding unit format.
+     */
+    UnitFormat getUnitFormat(Locale locale);
+
+}
