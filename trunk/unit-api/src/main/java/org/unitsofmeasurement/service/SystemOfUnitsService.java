d2865a10bc69eb39cf073d7d89a2fd342d58317e
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2010-10-03 18:30:19 +0000

    NEW - task 5: Refactoring http://code.google.com/p/unitsofmeasure/issues/detail?id=5

--- trunk/unit-api/src/main/java/org/unitsofmeasurement/service/SystemOfUnitsService.java
+++ trunk/unit-api/src/main/java/org/unitsofmeasurement/service/SystemOfUnitsService.java
@@ -10,24 +10,30 @@ package org.unitsofmeasurement.service;
 import org.unitsofmeasurement.unit.SystemOfUnits;
 
 /**
- * <p> This interface represents the service to obtain a {@link SystemOfUnits
- *     system of units}.</p>
- *
- * <p> Common system of units are "SI" (System International), "Imperial"
- *     (British),  "USCustomary".</p>
- *
+ * <p>
+ * This interface represents the service to obtain a {@link SystemOfUnits system
+ * of units}.
+ * </p>
+ * 
+ * <p>
+ * Common system of units are "SI" (System International), "Imperial" (British),
+ * "USCustomary".
+ * </p>
+ * 
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 1.2.1 ($Revision$), $Date$
+ * @version 1.2.2 ($Revision$), $Date: 2010-09-28 16:16:43 +0200 (Di, 28
+ *          Sep 2010) $
  */
 public interface SystemOfUnitsService {
 
-    /**
-     * Returns the default <a href=http://en.wikipedia.org/wiki/International_System_of_Units">
-     * International System of Units</a>.
-     *
-     * @return <code>getSystemOfUnits("SI")</code>
-     */
+	/**
+	 * Returns the default <a
+	 * href=http://en.wikipedia.org/wiki/International_System_of_Units">
+	 * International System of Units</a>.
+	 * 
+	 * @return <code>getSystemOfUnits("SI")</code>
+	 */
     SystemOfUnits getSystemOfUnits();
 
     /**
