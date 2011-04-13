19ff2129c2207d639ec1b5fd8ef70b130e183c05
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2011-04-13 19:17:16 +0000

    Updated JavaDoc (Wikipedia)

--- unit-api/src/main/java/org/unitsofmeasurement/quantity/Information.java
+++ unit-api/src/main/java/org/unitsofmeasurement/quantity/Information.java
@@ -1,20 +1,22 @@
 /**
  * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
- * Copyright (c) 2005-2010, Unit-API contributors, JScience and others
+ * Copyright (c) 2005-2011, Unit-API contributors, JScience and others
  * All rights reserved.
  *
  * See LICENSE.txt for details.
  */
 package org.unitsofmeasurement.quantity;
 
-
 /**
- * Represents a measure of information. The metric system unit for this quantity is
- * "bit".
- *
+ * Represents a measure of information. The metric system unit for this quantity
+ * is "bit".
+ * 
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 1.1 ($Revision$), $Date$
+ * @version 1.2 ($Revision$), $Date: 2010-10-29 17:17:07 +0100 (Fr, 29 Okt
+ *          2010) $
+ * @see <a href="http://en.wikipedia.org/wiki/Units_of_information"> Wikipedia:
+ *      Units of Information</a>
  */
 public interface Information extends Quantity<Information> {
 
--- unit-api/src/main/java/org/unitsofmeasurement/quantity/InformationRate.java
+++ unit-api/src/main/java/org/unitsofmeasurement/quantity/InformationRate.java
@@ -1,20 +1,22 @@
 /**
  * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
- * Copyright (c) 2005-2010, Unit-API contributors, JScience and others
+ * Copyright (c) 2005-2011, Unit-API contributors, JScience and others
  * All rights reserved.
  *
  * See LICENSE.txt for details.
  */
 package org.unitsofmeasurement.quantity;
 
-
 /**
  * This interface represents the speed of data-transmission. The system unit for
  * this quantity is "bit/s" (bit per second).
- *
+ * 
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 1.2, $Date$
+ * @version 1.3, $Date$
+ * @see <a href="http://en.wikipedia.org/wiki/Information_rate"> Wikipedia:
+ *      Information Rate</a>
+ * 
  */
 public interface InformationRate extends Quantity<InformationRate> {
 }
