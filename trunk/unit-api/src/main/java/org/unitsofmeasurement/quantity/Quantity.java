8cfce31ea5e99c9ec8b37e8eed7b762d52abc187
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2011-11-21 22:52:27 +0000

    Tagged v0.6.0

--- trunk/unit-api/src/main/java/org/unitsofmeasurement/quantity/Quantity.java
+++ trunk/unit-api/src/main/java/org/unitsofmeasurement/quantity/Quantity.java
@@ -1,13 +1,11 @@
 /**
  * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
- * Copyright (c) 2005-2011, Unit-API contributors, JScience and others
+ * Copyright (c) 2005-2010, Unit-API contributors, JScience and others
  * All rights reserved.
  *
  * See LICENSE.txt for details.
  */
 package org.unitsofmeasurement.quantity;
-
-import org.unitsofmeasurement.unit.Unit;
 
 /**
  * <p>
@@ -17,40 +15,26 @@ import org.unitsofmeasurement.unit.Unit;
  * </p>
  *
  * <p>
- * This interface is used to specify the quantitative property associated to a
- * class through class parameterization and to provide limited compile time
- * dimension consistency.
- *
- * [code]
- * Unit<Mass> pound = ...
- * Quantity<Length> size = ...
- * Sensor<Temperature> thermometer = ...
- * Vector3D<Velocity> aircraftSpeed = ...
+ * This interface has no method (tagging interface) is used solely to specify
+ * the quantitative property associated to a class through class
+ * parameterization and to provide limited compile time dimension
+ * consistency.[code] Unit<Mass> pound = ... Quantity<Length> size = ...
+ * Sensor<Temperature> thermometer = ... Vector3D<Velocity> aircraftSpeed = ...
  * [/code]
  * </p>
  *
- * @param <Q> The type of the quantity.
+ * @param <Q>
+ *            The type of the quantity.
  *
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
- * @author <a href="mailto:desruisseaux@users.sourceforge.net">Martin Desruisseaux</a>
+ * @author <a href="mailto:desruisseaux@users.sourceforge.net">Martin
+ *         Desruisseaux</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
  * @see <a href="http://en.wikipedia.org/wiki/Quantity">Wikipedia: Quantity</a>
- * @version 1.3 ($Revision$), $Date$
+ * @version 1.2.1 ($Revision$), $Date$
  */
 public interface Quantity<Q extends Quantity<Q>> {
 
-    /**
-     * Returns the value of this quantity as a number stated in this
-     * quantity {@linkplain #unit() unit}.
-     *
-     * @return the value of this quantity (can not be {@code null}).
-     */
-    Number value();
+    // No method, tagging interface.
 
-    /**
-     * Returns the unit of this quantity {@linkplain #value() value}.
-     *
-     * @return the unit of this quantity (can not be {@code null}).
-     */
-    Unit<Q> unit();
 }
