8359083f2fda078ffc29b6c14f217a9120f998f9
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2011-11-21 22:36:01 +0000

    Tagged v0.6-RC3

---
+++ tags/v0.6-RC3/src/test/java/org/unitsofmeasurement/util/CircleInfo.java
@@ -0,0 +1,29 @@
+/**
+ * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
+ * Copyright (c) 2005-2010, Unit-API contributors, JScience and others
+ * All rights reserved.
+ *
+ * See LICENSE.txt for details.
+ */
+package org.unitsofmeasurement.util;
+
+import org.unitsofmeasurement.quantity.AreaQuantity;
+import org.unitsofmeasurement.quantity.DistanceQuantity;
+import org.unitsofmeasurement.unit.DistanceUnit;
+
+public class CircleInfo {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        DistanceQuantity radius = new DistanceQuantity(30, DistanceUnit.cm);
+        System.out.println("Radius = " + radius);
+        double mult = 2 * Math.PI;
+        DistanceQuantity circumference = radius.multiply(mult);
+        System.out.println("Circumference = " + circumference);
+        AreaQuantity area = (radius.multiply(radius)).multiply(Math.PI);
+        System.out.println("Area = " + area);
+    }
+
+}
