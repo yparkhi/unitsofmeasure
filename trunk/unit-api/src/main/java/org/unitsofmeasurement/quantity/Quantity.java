6fd18fc426a4b493f01bdf794113ebf0e2c652ff
martdesruisseaux <martdesruisseaux@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2011-11-24 02:28:41 +0000

    Reformating.

--- trunk/unit-api/src/main/java/org/unitsofmeasurement/quantity/Quantity.java
+++ trunk/unit-api/src/main/java/org/unitsofmeasurement/quantity/Quantity.java
@@ -15,41 +15,42 @@ import org.unitsofmeasurement.unit.Unit;
  * distance, heat, and angular separation are among the familiar examples of
  * quantitative properties.
  * </p>
- * 
+ *
  * <p>
  * This interface is used to specify the quantitative property associated to a
  * class through class parameterization and to provide limited compile time
  * dimension consistency.
- * 
- * [code] Unit<Mass> pound = ... Quantity<Length> size = ... Sensor<Temperature>
- * thermometer = ... Vector3D<Velocity> aircraftSpeed = ... [/code]
+ *
+ * [code]
+ * Unit<Mass> pound = ...
+ * Quantity<Length> size = ...
+ * Sensor<Temperature> thermometer = ...
+ * Vector3D<Velocity> aircraftSpeed = ...
+ * [/code]
  * </p>
- * 
- * @param <Q>
- *            The type of the quantity.
- * 
+ *
+ * @param <Q> The type of the quantity.
+ *
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
- * @author <a href="mailto:desruisseaux@users.sourceforge.net">Martin
- *         Desruisseaux</a>
+ * @author <a href="mailto:desruisseaux@users.sourceforge.net">Martin Desruisseaux</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
  * @see <a href="http://en.wikipedia.org/wiki/Quantity">Wikipedia: Quantity</a>
- * @version 1.3 ($Revision$), $Date: 2011-11-01 12:21:06 +0100 (Di, 01 Nov
- *          2011) $
+ * @version 1.3 ($Revision$), $Date$
  */
 public interface Quantity<Q extends Quantity<Q>> {
 
-	/**
-	 * Returns the value of this quantity as a number stated in this quantity
-	 * {@linkplain #unit() unit}.
-	 * 
-	 * @return the value of this quantity (can not be {@code null}).
-	 */
-	Number value();
+    /**
+     * Returns the value of this quantity as a number stated in this quantity
+     * {@linkplain #unit() unit}.
+     *
+     * @return the value of this quantity (can not be {@code null}).
+     */
+    Number value();
 
-	/**
-	 * Returns the unit of this quantity {@linkplain #value() value}.
-	 * 
-	 * @return the unit of this quantity (can not be {@code null}).
-	 */
-	Unit<Q> unit();
+    /**
+     * Returns the unit of this quantity {@linkplain #value() value}.
+     *
+     * @return the unit of this quantity (can not be {@code null}).
+     */
+    Unit<Q> unit();
 }
