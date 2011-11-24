11b5a39202d716629c0ddc7224a8cc392d4635fc
martdesruisseaux <martdesruisseaux@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2011-11-24 02:28:41 +0000

    Reformating.

--- unit-api/src/main/java/org/unitsofmeasurement/unit/IncommensurableException.java
+++ unit-api/src/main/java/org/unitsofmeasurement/unit/IncommensurableException.java
@@ -12,20 +12,18 @@ package org.unitsofmeasurement.unit;
  * some quantities/units. Only commensurable quantity (quantities with the same
  * dimensions) may be compared, equated, added, or subtracted. Also, one unit
  * can be converted to another unit only if both units are commensurable.
- * 
+ *
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:jcp@catmedia.us">Werner Keil</a>
- * @see <a
- *      href="http://en.wikipedia.org/wiki/Unit_commensurability#Commensurability">
+ * @see <a href="http://en.wikipedia.org/wiki/Unit_commensurability#Commensurability">
  *      Wikipedia: Unit Commensurability</a>
- * @version 1.2 ($Revision$), $Date: 2011-10-27 23:18:24 +0200 (Do, 27 Okt
- *          2011) $
+ * @version 1.2 ($Revision$), $Date$
  */
 public class IncommensurableException extends Exception {
 
     /**
-	 * For cross-version compatibility.
-	 */
+     * For cross-version compatibility.
+     */
     private static final long serialVersionUID = -3676414292638136515L;
 
     /**
@@ -35,6 +33,6 @@ public class IncommensurableException extends Exception {
      * @param message the detail message.
      */
     public IncommensurableException(String message) {
-		super(message);
+        super(message);
     }
 }
