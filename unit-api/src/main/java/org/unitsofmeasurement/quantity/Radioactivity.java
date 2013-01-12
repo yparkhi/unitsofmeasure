ac8fc970e70e8891275670fcf532a37da90576de
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2013-01-12 01:09:26 +0000

    ASSIGNED - Part of "Cleaning org.unitsofmeasurement"

--- unit-api/src/main/java/org/unitsofmeasurement/quantity/Radioactivity.java
+++ unit-api/src/main/java/org/unitsofmeasurement/quantity/Radioactivity.java
@@ -1,6 +1,6 @@
 /**
  * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
- * Copyright (c) 2005-2010, Unit-API contributors, JScience and others
+ * Copyright (c) 2005-2013, Unit-API contributors, JScience and others
  * All rights reserved.
  *
  * See LICENSE.txt for details.
@@ -9,12 +9,13 @@ package org.unitsofmeasurement.quantity;
 
 
 /**
- * Radioactive activity.
- * The metric system unit for this quantity is "Bq" (Becquerel).
- *
+ * Radioactive activity. The metric system unit for this quantity is "Bq"
+ * (Becquerel).
+ * 
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 1.1
+ * @author Martin Desruisseaux
+ * @version 1.2
  */
 public interface Radioactivity extends Quantity<Radioactivity> {
 }
