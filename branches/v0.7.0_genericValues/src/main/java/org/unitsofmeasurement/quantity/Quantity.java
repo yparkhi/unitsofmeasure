7da9655e5e8fd642556e4e07639f178a64c8119a
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2013-01-17 19:07:55 +0000

    Updated Quantity

--- branches/v0.7.0_genericValues/src/main/java/org/unitsofmeasurement/quantity/Quantity.java
+++ branches/v0.7.0_genericValues/src/main/java/org/unitsofmeasurement/quantity/Quantity.java
@@ -1,13 +1,13 @@
 /**
  * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
- * Copyright (c) 2005-2012, Unit-API contributors, JScience and others
+ * Copyright (c) 2005-2013, Unit-API contributors, JScience and others
  * All rights reserved.
  *
  * See LICENSE.txt for details.
  */
 package org.unitsofmeasurement.quantity;
 
-import org.unitsofmeasurement.unit.Unit;
+import org.unitsofmeasurement.unit.UnitProvider;
 
 /**
  * <p>
@@ -27,18 +27,16 @@ import org.unitsofmeasurement.unit.Unit;
  * 
  * @param <Q>
  *            The type of the quantity.
- * @param <V>
- *            The value of the quantity.
  * 
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:desruisseaux@users.sourceforge.net">Martin
  *         Desruisseaux</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
  * @see <a href="http://en.wikipedia.org/wiki/Quantity">Wikipedia: Quantity</a>
- * @version 1.5 ($Revision$), $Date: 2012-04-30 01:07:51 +0200 (Mo, 30 Apr
+ * @version 1.6 ($Revision$), $Date: 2012-04-30 01:07:51 +0200 (Mo, 30 Apr
  *          2012) $
  */
-public interface Quantity<Q extends Quantity<Q>> {
+public interface Quantity<Q extends Quantity<Q>> extends UnitProvider<Q> {
 
     /**
 	 * Returns the value of this quantity {@linkplain #unit() unit}.
@@ -46,11 +44,4 @@ public interface Quantity<Q extends Quantity<Q>> {
 	 * @return the value of this quantity (can not be {@code null}).
 	 */
 	// V value();
-
-    /**
-     * Returns the unit of this quantity {@linkplain #value() value}.
-     *
-     * @return the unit of this quantity (can not be {@code null}).
-     */
-	Unit<Q> unit();
 }
