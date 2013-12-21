a4a68eb238c46b22c6b4c64c59acc7dddf7f8ea5
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2013-12-21 00:22:34 +0000

    Initial import.

---
+++ branches/explorations/spec_lead_draft/service/src/main/java/javax/measure/service/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Unit-API - Units of Measurement API for Java (http://unitsofmeasurement.org)
+ * Copyright (c) 2005-2010, Unit-API contributors, JScience and others
+ * All rights reserved.
+ *
+ * See LICENSE.txt for details.
+ */
+/**
+ * Provides services for units, quantities or their textual representation.
+ * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
+ * @author  <a href="mailto:units@catmedia.us">Werner Keil</a>
+ * @version 1.0, $Date: 2010-10-29 18:17:07 +0200 (Fr, 29 Okt 2010) $
+ */
+package javax.measure.service;
