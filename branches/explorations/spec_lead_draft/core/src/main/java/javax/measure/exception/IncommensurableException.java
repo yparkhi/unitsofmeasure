f3bf6bfd23d1e5bd97b34c572c1482b98a228710
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2013-12-31 00:09:28 +0000

    Updated exception hierarchy

--- branches/explorations/spec_lead_draft/core/src/main/java/javax/measure/exception/IncommensurableException.java
+++ branches/explorations/spec_lead_draft/core/src/main/java/javax/measure/exception/IncommensurableException.java
@@ -13,10 +13,13 @@ package javax.measure.exception;
  * some quantities/units. Only commensurable quantity (quantities with the same
  * dimensions) may be compared, equated, added, or subtracted. Also, one unit
  * can be converted to another unit only if both units are commensurable.
- *
+ * <p>
+ * This is a <strong>checked</strong> exception, so it deliberately doesn't
+ * inherit from <code>MeasurementException</code> like most other exceptions.
+ * </p>
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 1.4
+ * @version 1.5, $Date$
  *
  * @see <a href="http://en.wikipedia.org/wiki/Unit_commensurability#Commensurability">Wikipedia: Unit Commensurability</a>
  */
