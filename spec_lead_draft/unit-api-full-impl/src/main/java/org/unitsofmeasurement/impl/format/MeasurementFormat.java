e84dc253a37d0da6d884c968f06b286a9ffcfa30
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2014-01-27 22:26:22 +0000

    ASSIGNED - bug 426621: JCP Proposal - updated license https://bugs.eclipse.org/bugs/show_bug.cgi?id=426621

--- spec_lead_draft/unit-api-full-impl/src/main/java/org/unitsofmeasurement/impl/format/MeasurementFormat.java
+++ spec_lead_draft/unit-api-full-impl/src/main/java/org/unitsofmeasurement/impl/format/MeasurementFormat.java
@@ -1,10 +1,17 @@
-/*
- * JScience - Java(TM) Tools and Libraries for the Advancement of Sciences.
- * Copyright (C) 2010-2013 - JScience (http://jscience.org/)
- * All rights reserved.
+/**
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright 2013-2014, Jean-Marie Dautelle, Werner Keil, V2COM and individual
+ *  contributors by the @author tag.
  *
- * Permission to use, copy, modify, and distribute this software is
- * freely granted, provided that this notice is preserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.unitsofmeasurement.impl.format;
 
