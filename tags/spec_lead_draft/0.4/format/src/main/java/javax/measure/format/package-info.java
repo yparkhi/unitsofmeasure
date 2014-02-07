1bbb32ff14116a22cc8bd2d904a0bf4c03cfc31f
werner.keil <werner.keil@cba44c62-c690-ceb6-94c0-d645b158c5e7>
2014-02-07 18:40:42 +0000

    ASSIGNED - bug 426621: JCP Proposal - tagging https://bugs.eclipse.org/bugs/show_bug.cgi?id=426621

---
+++ tags/spec_lead_draft/0.4/format/src/main/java/javax/measure/format/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Unit-API - Units of Measurement API for Java
+ * Copyright (c) 2014 Jean-Marie Dautelle, Werner Keil, V2COM
+ * All rights reserved.
+ *
+ * See LICENSE.txt for details.
+ */
+/**
+ * [OPTIONAL] Provides Formatting and Parsing functionality for units, quantities, dimensions or their textual representation.
+ * @author  <a href="mailto:units@catmedia.us">Werner Keil</a>
+ * @version 0.4.1, $Date$
+ */
+package javax.measure.format;
